9140d27cfe954895ea31123b0b14ab913ac53cba
Webnist <webnist@webnist.org>
2021-08-05 19:38:45 +0900

    TEST

---
+++ custom-nextpage.php
@@ -0,0 +1,248 @@
+<?php
+/*
+Plugin Name: Custom Nextpage
+Plugin URI: http://wordpress.org/plugins/custom-nextpage/
+Description: MultiPage is a customizable plugin. Can any title on the page.
+Author: Webnist
+Version: 1.1.1
+Author URI: http://profiles.wordpress.org/webnist
+License: GPLv2 or later
+Text Domain: custom-nextpage
+Domain Path: /languages/
+*/
+
+if ( !defined( 'CUSTOM_NEXTPAGE_DIR' ) )
+	define( 'CUSTOM_NEXTPAGE_DIR', WP_PLUGIN_DIR . '/custom-nextpage' );
+
+if ( !defined( 'CUSTOM_NEXTPAGE_URL' ) )
+	define( 'CUSTOM_NEXTPAGE_URL', WP_PLUGIN_URL . '/custom-nextpage' );
+
+if ( !class_exists('CustomNextPageAdmin') )
+	require_once(dirname(__FILE__).'/includes/class-admin-menu.php');
+
+if ( !class_exists('CustomNextPageEditor') )
+	require_once(dirname(__FILE__).'/includes/class-admin-editor.php');
+
+class CustomNextPageInit {
+
+	public function __construct() {
+		$data                  = get_file_data(
+			__FILE__,
+			array(
+				'ver'    => 'Version',
+				'domain' => 'Text Domain',
+				'langs'  => 'Domain Path'
+			)
+		);
+
+		$this->plugin_basename = dirname( plugin_basename(__FILE__) );
+		$this->version         = $data['ver'];
+		$this->domain          = $data['domain'];
+		$this->langs           = $data['langs'];
+		$this->css             = strip_tags( file_get_contents( CUSTOM_NEXTPAGE_DIR . '/css/custom-nextpage-style.css' ) );
+
+		$this->default_options = array(
+			'filter'           => '',
+			'beforetext'       => '',
+			'aftertext'        => '',
+			'show_all'         => 1,
+			'end_size'         => 1,
+			'mid_size'         => 2,
+			'show_boundary'    => 1,
+			'show_adjacent'    => 1,
+			'firstpagelink'    => __( '&#171;', 'custom-nextpage' ),
+			'lastpagelink'     => __( '&#187;', 'custom-nextpage' ),
+			'nextpagelink'     => __( '&gt;', 'custom-nextpage' ),
+			'previouspagelink' => __( '&lt;', 'custom-nextpage' ),
+			'styletype'        => 0,
+			'style'            => $this->css,
+		);
+		$this->options          = get_option( 'custom-next-page', $this->default_options );
+		$this->filter           = $this->options['filter'] ? $this->options['filter'] : '';
+		$this->beforetext       = $this->options['beforetext'] ? $this->options['beforetext'] : '';
+		$this->aftertext        = $this->options['aftertext'] ? $this->options['aftertext'] : '';
+		$this->show_all         = $this->options['show_all'] ? $this->options['show_all'] : '';
+		$this->end_size         = $this->options['end_size'] ? $this->options['end_size'] : 1;
+		$this->mid_size         = $this->options['mid_size'] ? $this->options['mid_size'] : 2;
+		$this->show_boundary    = $this->options['show_boundary'] ? $this->options['show_boundary'] : '';
+		$this->show_adjacent    = $this->options['show_adjacent'] ? $this->options['show_adjacent'] : '';
+		$this->firstpagelink    = $this->options['firstpagelink'] ? $this->options['firstpagelink'] : __( '&#171;', 'custom-nextpage' );
+		$this->lastpagelink     = $this->options['lastpagelink'] ? $this->options['lastpagelink'] : __( '&#187;', 'custom-nextpage' );
+		$this->nextpagelink     = $this->options['nextpagelink'] ? $this->options['nextpagelink'] : __( '&gt;', 'custom-nextpage' );
+		$this->previouspagelink = $this->options['previouspagelink'] ? $this->options['previouspagelink'] : __( '&lt;', 'custom-nextpage' );
+		$this->styletype        = $this->options['styletype'] ? $this->options['styletype'] : 0;
+		$this->style            = $this->options['style'] ? $this->options['style'] : $this->css;
+	}
+}
+
+class CustomNextPage extends CustomNextPageInit {
+
+	public function __construct() {
+		parent::__construct();
+
+		if ( !is_admin() ) {
+			add_action( 'loop_start', array( &$this, 'change_nextpage' ) );
+			if ( $this->filter === 1 )
+				add_filter( 'wp_link_pages', array( &$this, 'wp_link_pages' ) );
+			add_action( 'wp_enqueue_scripts', array( &$this, 'wp_enqueue_scripts' ) );
+		}
+		add_shortcode( 'nextpage', array( &$this, 'shortcode' ) );
+		load_plugin_textdomain( $this->domain, false, $this->plugin_basename . $this->langs );
+	}
+
+	public function plugins_loaded() {
+	}
+
+	public function change_nextpage( $query ) {
+		if ( is_feed() || is_404() )
+			return;
+
+		$posts       = $query->posts;
+		$pattern     = array( "/<.*?>\[nextpage\]<\/.*?>/", "/\[nextpage[^\]]*\]/" );
+		$replacement = array( '<!--nextpage-->', '<!--nextpage-->' );
+		$count = 0;
+		foreach ( $posts as $post ) {
+			$content = $post->post_content;
+			$query->posts[$count]->post_content = preg_replace( $pattern, $replacement, $content );
+			$count++;
+		}
+		return $query;
+	}
+
+	public function next_page_title( $id = '' ) {
+		global $page, $numpages, $multipage;
+
+		if ( !$id )
+			$id = get_the_id();
+
+		$output = '';
+		if ( $multipage ) {
+			$pattern      = "/\[nextpage[^\]]*\]/";
+			$post         = get_post( $id );
+			$content      = $post->post_content;
+			$matche_count = $page - 1;
+			$page_count   = $page + 1;
+			preg_match_all( $pattern, $content, $matches );
+			$page_title = isset( $matches[0][$matche_count] ) ? $matches[0][$matche_count] : '';
+			if ( $page_title ) {
+				$pattern = '/title=["?](.*)["?]/';
+				preg_match( $pattern, $page_title, $matches);
+				$title  = isset( $matches[1] ) ? esc_html( $matches[1] ) : '';
+				$before = apply_filters( 'custom_next_page_beforetext', $this->beforetext );
+				$after  = apply_filters( 'custom_next_page_aftertext', $this->aftertext );
+				$output .= '<p class="custom-page-links">' ."\n";
+				if ( $page_count <= $numpages ) {
+					$output .= _wp_link_page( $page_count );
+					$output .= $before . $title . $after . '</a>';
+				}
+				$output .= '</p>' ."\n";
+			}
+		}
+		return $output;
+	}
+
+	public function wp_link_pages( $output = '' ) {
+		global $page, $numpages, $multipage, $pagenow;
+		$output = '';
+		if ( $multipage ) {
+			$show_all         = (boolean) $this->show_all;
+			$end_size         = $this->end_size;
+			$mid_size         = $this->mid_size;
+			$show_boundary    = esc_html( apply_filters( 'custom_next_page_show_boundary', $this->show_boundary ) );
+			$show_adjacent    = esc_html( apply_filters( 'custom_next_page_show_adjacent', $this->show_adjacent ) );
+			$firstpagelink    = esc_html( apply_filters( 'custom_next_page_firstpagelink', $this->firstpagelink ) );
+			$lastpagelink     = esc_html( apply_filters( 'custom_next_page_lastpagelink', $this->lastpagelink ) );
+			$nextpagelink     = esc_html( apply_filters( 'custom_next_page_nextpagelink', $this->nextpagelink ) );
+			$previouspagelink = esc_html( apply_filters( 'custom_next_page_previouspagelink', $this->previouspagelink ) );
+			$id               = get_the_ID();
+			$next_page_title  = self::next_page_title( $id );
+
+			$output .= '<div class="page-link-box">' ."\n";
+			$output .= $next_page_title;
+			$output .= '<ul class="page-links">' ."\n";
+			$i = $page - 1;
+
+			if ( $page > 1 ) {
+				$first_link = _wp_link_page( 1 );
+				$link       = _wp_link_page( $i );
+				if ( $show_boundary )
+					$output .= '<li class="first">' . $first_link . $firstpagelink . '</a></li>';
+				if ( $show_adjacent )
+					$output .= '<li class="previous">' . $link . $previouspagelink . '</a></li>';
+			}
+
+			$p_base   = get_permalink();
+			$p_format = '%#%';
+
+			if($word = strpos($p_base, '?')){
+				$p_base = get_option(home).(substr(get_option(home), -1 ,1) === '/' ? '' : '/')
+					.'%_%'.substr($p_base, $word);
+			} else{
+				$p_base .= (substr($p_base, -1 ,1) === '/' ? '' : '/') .'%_%';
+			}
+			$nav_list = paginate_links(array(
+				'base'      => $p_base,
+				'format'    => $p_format,
+				'total'     => $numpages,
+				'current'   => ($page ? $page : 1),
+				'show_all'  => $show_all,
+				'end_size'  => $end_size,
+				'mid_size'  => $mid_size,
+				'prev_next' => false,
+				'type'      => 'array',
+			));
+			foreach ( $nav_list as $nav ) {
+				if ( stristr( $nav, 'span' ) ) {
+					if ( stristr( $nav, 'dots' ) ) {
+						$output .= '<li class="numpages dots"><span>' . strip_tags( $nav ) . '</span></li>';
+					} else {
+						$output .= '<li class="numpages current"><span>' . strip_tags( $nav ) . '</span></li>';
+					}
+				} else {
+					$output .= '<li class="numpages">' . $nav . '</li>';
+				}
+			}
+
+			$i = $page + 1;
+			if ( $i <= $numpages ) {
+				$last_link = _wp_link_page( $numpages );
+				$link      = _wp_link_page( $i );
+				if ( $show_adjacent )
+					$output .= '<li class="next">' . $link . $nextpagelink . '</a></li>';
+
+				if ( $show_boundary )
+					$output .= '<li class="last">' . $last_link . $lastpagelink . '</a></li>';
+			}
+			$output .= '</ul>' ."\n";
+			$output .= '</div>' ."\n";
+		}
+		return $output;
+	}
+
+	public function wp_enqueue_scripts() {
+		if ( 0 == $this->styletype ) {
+			wp_enqueue_style( 'custom-nextpage-style', CUSTOM_NEXTPAGE_URL . '/css/custom-nextpage-style.css', array(), $this->version );
+		} elseif ( 1 == $this->styletype ) {
+			$print_html = sprintf( '<style type="text/css" id="custom-nextpage-style">' . "\n"
+				. '%s'
+				. "\n" . '</style>' . "\n",
+				$this->style
+			);
+			echo $print_html;
+		}
+	}
+
+	public function shortcode() {
+		return;
+	}
+}
+
+new CustomNextPageInit();
+new CustomNextPage();
+new CustomNextPageAdmin();
+new CustomNextPageEditor();
+
+function custom_next_page_link_pages() {
+	$custom_next_page = new CustomNextPage();
+	echo $custom_next_page->wp_link_pages();
+}
---
+++ includes/class-admin-menu.php
@@ -0,0 +1,379 @@
+<?php
+class CustomNextPageAdmin extends CustomNextPageInit {
+
+	public function __construct() {
+		parent::__construct();
+
+		add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
+		add_action( 'admin_enqueue_scripts', array( &$this, 'admin_enqueue_scripts' ) );
+		add_action( 'admin_init', array( &$this, 'add_general_custom_fields' ) );
+		add_filter( 'admin_init', array( &$this, 'add_custom_whitelist_options_fields' ) );
+		add_action( 'wp_ajax_reset_css', array( $this, 'reset_css') );
+	}
+
+	public function admin_menu() {
+		add_options_page( __( 'Custom Nextpage', $this->domain ), __( 'Custom Nextpage', $this->domain ), 'create_users', $this->plugin_basename, array( &$this, 'add_admin_edit_page' ) );
+	}
+
+	public function admin_enqueue_scripts( $hook ) {
+
+		if ( stristr( $hook, $this->plugin_basename ) ) {
+			wp_enqueue_style( 'options-customnextpage', CUSTOM_NEXTPAGE_URL . '/css/options.css', array(), $this->version );
+			wp_enqueue_style( 'codemirror', CUSTOM_NEXTPAGE_URL . '/includes/codemirror/lib/codemirror.css', array(), $this->version );
+			wp_enqueue_style( 'codemirror-show-hint', CUSTOM_NEXTPAGE_URL . '/includes/codemirror/addon/hint/show-hint.css', array(), $this->version );
+			wp_enqueue_style( 'codemirror-style', CUSTOM_NEXTPAGE_URL . '/css/codemirror-style.css', array(), $this->version );
+
+			wp_enqueue_script( 'codemirror', CUSTOM_NEXTPAGE_URL . '/includes/codemirror/lib/codemirror.js', array(), $this->version, true );
+			wp_enqueue_script( 'codemirror-show-hint', CUSTOM_NEXTPAGE_URL . '/includes/codemirror/addon/hint/show-hint.js', array('codemirror'), $this->version, true );
+			wp_enqueue_script( 'codemirror-css-hint', CUSTOM_NEXTPAGE_URL . '/includes/codemirror/addon/hint/css-hint.js', array('codemirror'), $this->version, true );
+			wp_enqueue_script( 'codemirror-mode-css', CUSTOM_NEXTPAGE_URL . '/includes/codemirror/mode/css/css.js', array('codemirror'), $this->version, true );
+			wp_enqueue_script( 'options-customnextpage', CUSTOM_NEXTPAGE_URL . '/js/options.js', array('jquery'), $this->version, true );
+
+			wp_localize_script(
+				'options-customnextpage',
+				'resetCss',
+				array(
+					'url'      => admin_url( 'admin-ajax.php' ),
+					'security' => wp_create_nonce( 'reset-css' )
+				)
+			);
+		}
+	}
+
+	public function add_admin_edit_page() {
+		$title = __( 'Set Custom Nextpage', $this->domain );
+		echo '<div class="wrap" id="custom-next-page-options">' . "\n";
+		screen_icon();
+		echo '<h2>' . esc_html( $title ) . '</h2>' . "\n";
+		echo '<form method="post" action="options.php">' . "\n";
+		settings_fields( $this->plugin_basename  );
+		do_settings_sections( $this->plugin_basename  );
+		echo '<div class="submit">' . "\n";
+		submit_button();
+		if ( get_option( 'custom-next-page-previouspagelink' ) ) {
+			echo '<h2>' . esc_html__( 'Convert to new options', $this->domain ) . '</h2>' . "\n";
+			submit_button( __( 'Convert', $this->domain ), 'primary', 'custom-next-page-convert' );
+		}
+		echo '<h2>' . esc_html__( 'Setting initialization', $this->domain ) . '</h2>' . "\n";
+		submit_button( __( 'Initialization', $this->domain ), 'primary', 'custom-next-page-initialization' );
+		echo '</div>' . "\n";
+		echo '</form>' . "\n";
+		echo '</div>' . "\n";
+	}
+
+	public function add_general_custom_fields() {
+		global $wp_version;
+
+		add_settings_section(
+			'general',
+			__( 'General', $this->domain ),
+			'',
+			$this->domain
+		);
+		if ( version_compare( $wp_version, '3.6', '>=' ) ) {
+			add_settings_field(
+				'custom-next-page-filter',
+				__( 'Automatically replace the wp_link_pages.', $this->domain ),
+				array( &$this, 'check_field' ),
+				$this->plugin_basename,
+				'general',
+				array(
+					'name'  => 'custom-next-page[filter]',
+					'value' => $this->filter,
+				)
+			);
+		}
+		add_settings_field(
+			'custom-next-page-before-text',
+			__( 'Before Text', $this->domain ),
+			array( &$this, 'text_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[beforetext]',
+				'value' => $this->beforetext,
+			)
+		);
+		add_settings_field(
+			'custom-next-page-after-text',
+			__( 'After Text', $this->domain ),
+			array( &$this, 'text_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[aftertext]',
+				'value' => $this->aftertext,
+			)
+		);
+
+		add_settings_field(
+			'custom-next-page-show_all',
+			__( 'Show all numbers', $this->domain ),
+			array( &$this, 'check_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[show_all]',
+				'value' => $this->show_all,
+			)
+		);
+		add_settings_field(
+			'custom-next-page-end_size',
+			__( 'End size', $this->domain ),
+			array( &$this, 'text_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[end_size]',
+				'value' => $this->end_size,
+				'class' => 'small-text',
+				'desc'  => __( 'How many numbers on either the start and the end list edges.', $this->domain ),
+			)
+		);
+		add_settings_field(
+			'custom-next-page-mid_size',
+			__( 'Mid size', $this->domain ),
+			array( &$this, 'text_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[mid_size]',
+				'value' => $this->mid_size,
+				'class' => 'small-text',
+				'desc'  => __( 'How many numbers to either side of current page, but not including current page.', $this->domain ),
+			)
+		);
+		add_settings_field(
+			'custom-next-page-boundary',
+			__( 'The first and last page links displayed.', $this->domain ),
+			array( &$this, 'check_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[show_boundary]',
+				'value' => $this->show_boundary,
+			)
+		);
+
+		add_settings_field(
+			'custom-next-page-adjacent',
+			__( 'Next and previous page links to display.', $this->domain ),
+			array( &$this, 'check_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[show_adjacent]',
+				'value' => $this->show_adjacent,
+			)
+		);
+
+		add_settings_field(
+			'custom-next-page-firstpagelink',
+			__( 'Text For First Page', $this->domain ),
+			array( &$this, 'text_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[firstpagelink]',
+				'value' => $this->firstpagelink,
+			)
+		);
+
+		add_settings_field(
+			'custom-next-page-lastpagelink',
+			__( 'Text For Last Page', $this->domain ),
+			array( &$this, 'text_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[lastpagelink]',
+				'value' => $this->lastpagelink,
+			)
+		);
+
+		add_settings_field(
+			'custom-next-page-nextpagelink',
+			__( 'Text For Next Page', $this->domain ),
+			array( &$this, 'text_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[nextpagelink]',
+				'value' => $this->nextpagelink,
+			)
+		);
+
+		add_settings_field(
+			'custom-next-page-previouspagelink',
+			__( 'Text For Previous Page', $this->domain ),
+			array( &$this, 'text_field' ),
+			$this->plugin_basename,
+			'general',
+			array(
+				'name'  => 'custom-next-page[previouspagelink]',
+				'value' => $this->previouspagelink,
+			)
+		);
+
+		add_settings_section(
+			'style',
+			__( 'Style', $this->domain ),
+			'',
+			$this->domain
+		);
+
+		add_settings_field(
+			'custom-next-page-style-type',
+			__( 'Select Style type', $this->domain ),
+			array( &$this, 'select_field' ),
+			$this->plugin_basename,
+			'style',
+			array(
+				'name'   => 'custom-next-page[styletype]',
+				'option' => array(
+					'0'  => __( 'Default', $this->domain ),
+					'1'  => __( 'Style Edit', $this->domain ),
+					'2'  => __( 'Disable', $this->domain ),
+				),
+				'id'    => 'styletype',
+				'value'  => $this->styletype,
+			)
+		);
+		add_settings_field(
+			'custom-next-page-style',
+			__( 'Style Edit', $this->domain ),
+			array( &$this, 'textarea_field' ),
+			$this->plugin_basename,
+			'style',
+			array(
+				'id'    => 'style-editor',
+				'name'  => 'custom-next-page[style]',
+				'value' => $this->style,
+				'desc'  => __( 'Press ctrl-space to activate autocompletion. <span class="button button-primary" id="reset-css">Reset</span>', $this->domain ),
+			)
+		);
+	}
+
+	public function text_field( $args ) {
+		extract( $args );
+
+		$id      = ! empty( $id ) ? $id : $name;
+		$desc    = ! empty( $desc ) ? $desc : '';
+		$class   = ! empty( $class ) ? $class: 'regular-text';
+		$output  = '<input type="text" name="' . $name .'" id="' . $id .'" class="' . $class . '" value="' . $value .'" />' . "\n";
+		if ( $desc )
+			$output .= '<p class="description">' . $desc . '</p>' . "\n";
+
+		echo $output;
+	}
+
+	public function textarea_field( $args ) {
+		extract( $args );
+
+		$id      = ! empty( $id ) ? $id : $name;
+		$desc    = ! empty( $desc ) ? $desc : '';
+		$output  = '<textarea name="' . $name .'" rows="10" cols="50" id="' . $id .'" class="large-text code">' . $value . '</textarea>' . "\n";
+		if ( $desc )
+			$output .= '<p class="description">' . $desc . '</p>' . "\n";
+		echo $output;
+	}
+
+	public function check_field( $args ) {
+		extract( $args );
+
+		$id      = ! empty( $id ) ? $id : $name;
+		$desc    = ! empty( $desc ) ? $desc : '';
+		$output  = '<label for="' . $name . '">' . "\n";
+		$output  .= '<input name="' . $name . '" type="checkbox" id="' . $id . '" value="1"' . checked( $value, 1, false ) . '>' . "\n";
+		if ( $desc )
+			$output .= $desc . "\n";
+		$output  .= '</label>' . "\n";
+
+		echo $output;
+	}
+
+	public function select_field( $args ) {
+		extract( $args );
+
+		$id             = ! empty( $id ) ? $id : $name;
+		$desc           = ! empty( $desc ) ? $desc : '';
+		$multi          = ! empty( $multi ) ? ' multiple' : '';
+		$multi_selected = ! empty( $multi ) ? true : false;
+		$output = '<select name="' . $name . '" id="' . $id . '"' . $multi . '>' . "\n";
+			foreach ( $option as $key => $val ) {
+				$output .= '<option value="' . $key . '"' . selected( $value, $key, $multi_selected ) . '>' . $val . '</option>' . "\n";
+			}
+		$output .= '</select>' . "\n";
+			if ( $desc )
+			$output .= $desc . "\n";
+
+		echo $output;
+	}
+
+	public function selected( $value = '', $val = '', $multi = false ) {
+		$select = '';
+		if ( $multi ) {
+
+			$select = selected( true, in_array( $val, $value ), false );
+		} else {
+			$select = selected( $value, $val, false );
+		}
+		return $select;
+	}
+
+	public function reset_css() {
+		check_ajax_referer( 'reset-css', 'security' );
+		$return = array(
+			'style' => $this->css
+		);
+		wp_send_json( $return );
+	}
+
+	public function add_custom_whitelist_options_fields() {
+		register_setting( $this->plugin_basename, 'custom-next-page', array( &$this, 'register_setting_check' ) );
+		register_setting( $this->plugin_basename, 'custom-next-page-convert', array( &$this, 'register_setting_convert' ) );
+		register_setting( $this->plugin_basename, 'custom-next-page-initialization', array( &$this, 'register_setting_initialization' ) );
+	}
+
+	public function register_setting_check( $value ) {
+		$value['filter']        = (int) $value['filter'];
+		$value['show_all']      = (int) $value['show_all'];
+		$value['end_size']      = (int) $value['end_size'];
+		$value['end_size']      = (int) $value['end_size'];
+		$value['show_boundary'] = (int) $value['show_boundary'];
+		$value['show_adjacent'] = (int) $value['show_adjacent'];
+		$value['style']         = preg_replace( '/(\&lt;(.*)\&gt;)/ism', '', esc_textarea( $value['style'] ) );
+		return $value;
+	}
+
+	public function register_setting_convert( $value ) {
+		if ( __( 'Convert', $this->domain ) != $value )
+			return $value;
+
+		$convert_options                     = get_option( 'custom-next-page' );
+		$convert_options['filter']           = get_option( 'custom-next-page-filter' );
+		$convert_options['before-text']      = get_option( 'custom-next-page-before-text' );
+		$convert_options['after-text']       = get_option( 'custom-next-page-after-text' );
+		$convert_options['nextpagelink']     = get_option( 'custom-next-page-nextpagelink', __( '&#187;', $this->domain ) );
+		$convert_options['previouspagelink'] = get_option( 'custom-next-page-previouspagelink', __( '&#171;', $this->domain ) );
+		update_option( 'custom-next-page', $convert_options );
+		delete_option( 'custom-next-page-filter' );
+		delete_option( 'custom-next-page-before-text' );
+		delete_option( 'custom-next-page-after-text' );
+		delete_option( 'custom-next-page-firstpagelink' );
+		delete_option( 'custom-next-page-lastpagelink' );
+		delete_option( 'custom-next-page-nextpagelink' );
+		delete_option( 'custom-next-page-previouspagelink' );
+		return $value;
+	}
+
+	public function register_setting_initialization( $value ) {
+		if ( __( 'Initialization', $this->domain ) != $value )
+			return $value;
+
+		delete_option( 'custom-next-page' );
+		update_option( 'custom-next-page', $this->default_options );
+		return $value;
+	}
+
+}
