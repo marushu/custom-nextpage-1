67fc5c8d36dcfaab5d4b508cd3267a5ba4af1a88
Shuhei <nishimura@digitalcube.jp>
2021-12-30 02:20:43 +0900

    package.json 編集とビルド

--- block/build/index.asset.php
+++ block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '311e8d33e849e28fa67efab5c93141b6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6ca37b4b1979fad6f3c2d4ccb4736fe0');
